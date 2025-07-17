46b936ecab4574fb9e5295c722ee302ec9300e41
lovee <3942121+el-hoshino@users.noreply.github.com>
2025-07-18 04:02:28 +0900

    Styleを設定

--- book/vivliostyle.config.js
+++ book/vivliostyle.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: 'ゆめみ大技林 ' /*\'23'*/,
-  author: 'ゆめみ大技林製作委員会',
+  // title: 'ゆめみ大技林 ' /*\'23'*/,
+  // author: 'ゆめみ大技林製作委員会',
   language: 'ja',
-  size: 'A5',
+  size: 'A4',
   theme: [
     'vivliostyle-theme-macneko-techbook@0.4.0',
     '@mitsuharu/vivliostyle-theme-noto-sans-jp@0.1.4',
-    'theme/theme-custom',
+    'theme/print.css',
   ],
   entry: [
     // 目次
